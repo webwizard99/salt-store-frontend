5016569f2606660b673529c680837923a6628fbc
Michael Bragg <michael_bragg99@hotmail.com>
2019-11-16 03:44:20 -0700

    Install react-router-dom and create placeholder components.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import './App.css';
+
+import { BrowserRouter, Route, withRouter } from 'react-router-dom';
+
+const Home = () => (<div>Home</div>);
+const Store = () => (<div>Store</div>);
+const About = () => (<div>About</div>);
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App"></div>
+      <div className="App">
+        <BrowserRouter>
+          <div>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/store" component={Store} />
+            <Route exact path="/about" component={About} />
+          </div>
+        </BrowserRouter>
+
+
+      </div>
     )
   }
 }
