95e52c2b95044d1cc888a12eba7446807ed2296c
Michael Bragg <michael_bragg99@hotmail.com>
2019-11-19 08:05:07 -0700

    Complete static home and about pages.

--- src/Containers/About/About.jsx
+++ src/Containers/About/About.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './About.css';
 
 import AboutBanner from '../../Components/AboutBanner/AboutBanner';
+import AboutStory from '../../Components/AboutStory/AboutStory';
 
 class About extends React.Component {
   render() {
     return (
       <div className="About">
         <AboutBanner />
+        <AboutStory />
       </div>
     );
   }
--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './Home.css';
 
 import HomeBanner from '../../Components/HomeBanner/HomeBanner';
+import HomeStory from '../../Components/HomeStory/HomeStory';
 
 class Home extends React.Component {
   render() {
     return (
       <div className="Home">
         <HomeBanner />
+        <HomeStory />
       </div>
     )
   }
