d8f4985e1ae92659d78b29b6efa648f5bc923cfd
Michael Bragg <michael_bragg99@hotmail.com>
2020-02-24 18:33:02 -0700

    Change text in profile link in Navbar to test build pipeline.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -16,7 +16,7 @@ class Navbar extends React.Component {
         <ul className="nav-options">
           <li><Link to="/store"><span>Store</span></Link></li>
           <li><Link to="/about"><span>About</span></Link></li>
-          <li><Link to="/profile"><span>Profile</span></Link></li>
+          <li><Link to="/profile"><span>O-&#60;</span></Link></li>
         </ul>
       </nav>
     )
