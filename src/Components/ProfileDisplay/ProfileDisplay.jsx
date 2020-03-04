9b66e0fb137c353f9aeb97b089d58a9455c33a01
Michael Bragg <michael_bragg99@hotmail.com>
2020-03-04 13:26:30 -0700

    Change profile icon in navbar to test pipeline.

---
+++ src/Components/ProfileDisplay/ProfileDisplay.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ProfileDisplay.css';
+
+class ProfileDisplay extends React.Component {
+  render() {
+    return (
+      <div className="ProfileDisplay container">
+        Your profile here.
+      </div>
+    )
+  }
+}
+
+export default ProfileDisplay;
