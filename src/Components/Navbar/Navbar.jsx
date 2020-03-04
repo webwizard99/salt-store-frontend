8c20298c9acbc9e8f40cb55b0b99e5ba61364f49
Michael Bragg <michael_bragg99@hotmail.com>
2020-03-04 13:34:58 -0700

    Change user icon in navbar to santa to test build pipeline.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -16,7 +16,7 @@ class Navbar extends React.Component {
         <ul className="nav-options">
           <li><Link to="/store"><span>Store</span></Link></li>
           <li><Link to="/about"><span>About</span></Link></li>
-          <li><Link to="/profile"><span>👤</span></Link></li>
+          <li><Link to="/profile"><span>🎅</span></Link></li>
         </ul>
       </nav>
     )
